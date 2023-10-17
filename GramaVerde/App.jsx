f84301af4eb3451fdf691fbdcac084529b207d49
gabeJSS <102593159+gabeJSS@users.noreply.github.com>
2023-10-17 13:17:20 -0300

    limpando o app.jsx

--- GramaVerde/App.jsx
+++ GramaVerde/App.jsx
@@ -1,18 +1,7 @@
-import { StyleSheet, Text, View } from 'react-native';
 import Routes from './src/routes/index.jsx';
-import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
       <Routes/>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
