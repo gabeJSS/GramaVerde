ae70bab5169c9db657d551a60c3d98a370bbb966
gabeJSS <102593159+gabeJSS@users.noreply.github.com>
2023-10-19 11:42:04 -0300

    Update App.jsx

--- GramaVerde/App.jsx
+++ GramaVerde/App.jsx
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
-import Routes from './src/routes/index.jsx'
-=======
 import Routes from './src/routes/index.jsx';
->>>>>>> f84301af4eb3451fdf691fbdcac084529b207d49
 
 export default function App() {
   return (
       <Routes/>
   );
-<<<<<<< HEAD
 }
-=======
-};
->>>>>>> f84301af4eb3451fdf691fbdcac084529b207d49
